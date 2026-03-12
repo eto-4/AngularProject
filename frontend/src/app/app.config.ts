81ded4a4a20b7f81d2b8624c9bfb62b556b286d1
Aleix <aleix.murt22@lacetania.cat>
2026-03-12 17:06:33 +0100

    canvis a dins de app.config (Registre de HttpClient per poder fer peticions HTTP als serveis)

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -1,11 +1,21 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { provideHttpClient } from '@angular/common/http';
 
 import { routes } from './app.routes';
 
+
+// Configuració principal de l'aplicació Angular (standalone)
+// Aquí registrem els providers globals: router i httpClient
 export const appConfig: ApplicationConfig = {
   providers: [
+    // Captura d'errors globals del navegador (nou a partir de angular +19)
     provideBrowserGlobalErrorListeners(),
-    provideRouter(routes)
+    
+    // Registrem les rutes de l'aplicació
+    provideRouter(routes),
+    
+    // Registrem HttpClient per poder fer peticions HTTP als serveis
+    provideHttpClient()
   ]
 };
