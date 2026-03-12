dc7829518bea1b26435566a8f5e35bcaba1030e8
Aleix <aleix.murt22@lacetania.cat>
2026-03-12 17:19:22 +0100

    Eliminació de component app.component (confusió)

--- frontend/src/app/app.component/app.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-app.component',
-  imports: [],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css',
-})
-export class AppComponent {}
