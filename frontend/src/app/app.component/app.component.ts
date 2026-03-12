20176d07522b4e17ccce64898dedd346da963479
Aleix <aleix.murt22@lacetania.cat>
2026-03-12 17:12:26 +0100

    Component base de l'app

---
+++ frontend/src/app/app.component/app.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-app.component',
+  imports: [],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css',
+})
+export class AppComponent {}
