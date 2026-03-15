5de521a4e4e6ea85105d68adc9ea7972aedbd71b
usuario <aleix.murt22@lacetania.cat>
2026-03-15 18:29:46 +0100

    Canvis mínims

--- frontend/src/app/components/login/login.component.ts
+++ frontend/src/app/components/login/login.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { Router, RouterLink } from '@angular/router';
+import { Router } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { ReactiveFormsModule, FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { AuthService } from '../../services/auth/auth.service';
@@ -8,8 +8,7 @@ import { AuthService } from '../../services/auth/auth.service';
     selector: 'app-login',
     imports: [
         CommonModule, 
-        ReactiveFormsModule, 
-        RouterLink
+        ReactiveFormsModule
     ],
     templateUrl: './login.component.html',
     styleUrl: './login.component.css',
